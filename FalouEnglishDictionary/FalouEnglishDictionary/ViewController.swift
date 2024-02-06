41133d5d2e1ec631c9e14f2bbe1a128c65854b3c
Gustavo Feliciano <gustavo.flcn@gmail.com>
2024-02-05 23:16:34 -0300

    Create xcode project

---
+++ FalouEnglishDictionary/FalouEnglishDictionary/ViewController.swift
@@ -0,0 +1,17 @@
+//
+// Created by Gustavo F. Figueiredo
+// Copyright © Gustavo F. Figueiredo. All Rights Reserved.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
